a22cee28a708c849b24c5c35e3c8048df04a2ec0
Ankita <ankitachoudhary73748@gmail.com>
2024-07-21 14:52:47 +0530

    added header

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  images: {
+    domains: ['s2.coinmarketcap.com', 's3.coinmarketcap.com', 'cdn.sanity.io', 'encrypted-tbn0.gstatic.com']
+  },
+
+
 };
 
 export default nextConfig;
